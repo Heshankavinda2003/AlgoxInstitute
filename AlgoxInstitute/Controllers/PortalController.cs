4bc64698e530f199d008b2393e292a011bc8741b
heshankav <heshankavinda2023@gmail.com>
2026-01-03 00:47:47 +0530

    Add PortalController for role-based user redirection
    Introduced PortalController with a RedirectUser action that routes users to different controllers based on their roles (Admin, Teacher, Student), defaulting to Home if no role matches. Comments note the need to implement CoursesController and StudentDashboardController for full support.

---
+++ AlgoxInstitute/Controllers/PortalController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlgoxInstitute.Controllers
+{
+	public class PortalController : Controller
+	{
+		public IActionResult RedirectUser()
+		{
+			if (User.IsInRole("Admin"))
+			{
+				return RedirectToAction("Index", "Admin");
+			}
+			else if (User.IsInRole("Teacher"))
+			{
+				// You need to create CoursesController
+				return RedirectToAction("Index", "Courses");
+			}
+			else if (User.IsInRole("Student"))
+			{
+				// You need to create DashboardController
+				return RedirectToAction("Index", "StudentDashboard");
+			}
+
+			return RedirectToAction("Index", "Home");
+		}
+	}
+}
